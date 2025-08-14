a71dbe84eb43e3c2de4e63ef59abf2b78a708876
Bonginkosi Moatshe <mobonginkosi@gmail.com>
2025-08-14 04:41:11 +0200

    Made a card game that lets the user select a card and the program throws random cards 20 times, if the users card is dealt the user wins.

--- Back2Basics/Program.cs
+++ Back2Basics/Program.cs
@@ -1,5 +1,8 @@
-﻿using System.Collections.Generic;
+﻿using System;
+using System.Collections.Generic;
 using System.Linq;
+using System.Runtime.CompilerServices;
+using System.Runtime.InteropServices;
 using System.Runtime.Remoting.Channels;
 using System.Security.Cryptography.X509Certificates;
 using System.Text;
@@ -11,15 +14,91 @@ namespace Back2Basics
     {
         static void Main(string[] args)
         {
-            LearningBasics.AbsoluteValue();
-            LearningBasics.Calculations();
-            LearningBasics.EmailChecker();
-            LearningBasics.AreaCalculator();
-            LearningBasics.Basics();
-            LearningBasics.Sum();
-            LearningBasics.Comments();
-            LearningBasics.DataTypes();
-            LearningBasics.Introduction();
+            Cards.GameLogic();
+            //Cards.UserSelection();
+            //LearningBasics.AbsoluteValue();
+            //LearningBasics.Calculations();
+            //LearningBasics.EmailChecker();
+            //LearningBasics.AreaCalculator();
+            //LearningBasics.Basics();
+            //LearningBasics.Sum();
+            //LearningBasics.Comments();
+            //LearningBasics.DataTypes();
+            //LearningBasics.Introduction();
+        }
+        public class Cards
+        {
+            public static string UserSelection()
+            {
+                Console.WriteLine("-----Select ace, number or face card----- ");
+                Console.Write("Please write the card in full(except for number cards): ");
+                string selectedCard = Console.ReadLine();
+                Console.WriteLine("-----Select a symbol----- ");
+                Console.Write("Please write the symbol in full: ");
+                string selectedSymbol = Console.ReadLine();
+                string userCard = selectedCard.ToUpper() + " of " + selectedSymbol.ToUpper();
+                
+                return userCard;
+               
+            }
+            private static readonly Random random = new Random();
+            public static string CompSelection()
+            {
+                
+                string[] cards = { "ace", "king", "queen", "jack", "two", "three", "four", "five", "six", "seven", "eight", "nine", "ten" };
+                string[] cardType = { "diamonds", "clubs", "hearts", "spades" };
+                int indexCard = random.Next(cards.Length);
+                int indexType = random.Next(cardType.Length);
+                string compCard = cards[indexCard].ToUpper() + " of " + cardType[indexType].ToUpper();
+
+                return compCard;
+            }
+
+            public static void GameLogic()
+            {
+                string userCard;
+                string compCard;
+                int chances;
+                int tries;
+                string playAgain = "x";
+
+                while (playAgain == "x")
+                { userCard = UserSelection();
+                    for (chances = 20; chances > 0; chances--)
+                    {
+                        compCard = CompSelection();
+                        if (compCard == userCard)
+                        {
+                            Console.WriteLine($"The user chose {userCard}");
+                            Console.WriteLine($"The computer chose {compCard}");
+                            tries = 20 - chances;
+                            if (tries == 1)
+                            {
+                                Console.WriteLine($"The user has won with {tries + 1} attempt");
+                            }
+                            else
+                            {
+                                Console.WriteLine($"The user has won with {tries + 1} attempts");
+                            }
+                            break;
+                        }
+                        else
+                        {
+                            Console.WriteLine($"No match!!! attempts left {chances - 1}");
+                            Console.WriteLine($"The computer chose {compCard}");
+                            if (chances == 1)
+                            {
+                                Console.WriteLine($"The user chose {userCard}");
+                                Console.WriteLine($"The computer chose {compCard}");
+                                Console.WriteLine("You've lost this round");
+                            }
+                        }
+                    }
+                    Console.WriteLine("Press x to play again or enter to quit");
+                    playAgain = Console.ReadLine();
+                } 
+            }
+
         }
     }       
 }
